a8262db3bad0bbfd19633cec5c63346c62f73ff5
marlonbarrios <marlon@dance-tech.net>
2021-09-22 21:31:16 -0400

    WIP: defining interactivity

--- js/script.js
+++ js/script.js
@@ -3,7 +3,6 @@ const $form = $('form');
 $form.on('submit', fetchData)
 
 function fetchData() {
-  console.log($form);
 
 
     //next iterate with a forEach
@@ -15,19 +14,24 @@ function fetchData() {
          (data) => { 
            console.log("fetched");
             data.forEach(function(coin, idx) {
-   // console.log(data);
-   // console.log(coin);
-   // console.log(idx);
-   
-   
+
              $(`#name-${idx} `).text(coin.id);
-             
+            
              $(`#price-${idx} `).text(coin.current_price);
             
             $(` #logo-${idx}`).html(`<p> <img class="image" src='${coin.image}'></p>`);
             })
+
+
            
-            
+            // data.forEach(function( onOff, idxe) {
+  
+            // $(`#name-${idxe} `).fadeOut( 500, function() {
+            // $(`#name-${idxe} `).fadeIn( 2000 );
+
+
+            // })
+
          },
         
          (error) => {
@@ -41,7 +45,46 @@ function fetchData() {
         fetchData();
    
    
-     // setInterval(() => {
-     //   fetchData();
-     // }, 60000);  
+        
+        // $( "input" ).click(function() {
+        //   $(`#name-${idx} `).fadeOut( 500, function() {
+        //     $(`#name-${idx} `).fadeIn( 2000 );
+            
+        //   });
+        //  return false;
+        // });
+
+
+        // $( "input" ).click(function() {
+        //   $( "#price-0" ).fadeOut( 500, function() {
+        //     $( "#price-0" ).fadeIn( 2000 );
+        //   });
+        //  return false;
+        // });
+
+
+
+
+       
+
+
+
+  //  setInterval(() => {
+  //   fetchData();
+    
+  //   $( "#name-0" ).fadeOut( 500, function() {
+  //     $( "#name-0" ).fadeIn( 2000 );
+  //   });
+  //   $( "#price-0" ).fadeOut( 500, function() {
+  //     $( "#price-0" ).fadeIn( 2000 );
+  //   });
+
+    
+  // }, 10000);  
+
+   
+//     //  $(`#name-${idx} `).text(coin.id).click(function() {
+//       $(`#name-${idx} `).text(coin.id);.first().fadeOut( "slow" );
+// // });
+
    
